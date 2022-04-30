c07ae91ad95a545b4a9c31f81d53b903eb6a7ede
dailam1994 <dai_lam88@hotmail.com>
2022-05-01 02:56:03 +1000

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { BrowserRouter } from "react-router-dom"
-import { registerSW } from "virtual:pwa-register"
+// import { registerSW } from "virtual:pwa-register"
 
 export const queryClient = new QueryClient()
 
 // Register Service Worker
-const updateSW = registerSW({
-   onOfflineReady() {},
-   onNeedRefresh() {},
-})
+// const updateSW = registerSW({
+//    onOfflineReady() {},
+//    onNeedRefresh() {},
+// })
 
 ReactDOM.render(
    <React.StrictMode>
--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
-module.exports = {
-   globDirectory: "dist/",
-   globPatterns: ["**/*.{png,js,css,svg,xml,ico,html,webmanifest,txt}"],
-   swDest: "dist/sw.js",
-   swSrc: "src-sw.js",
-}
+// module.exports = {
+//    globDirectory: "dist/",
+//    globPatterns: ["**/*.{png,js,css,svg,xml,ico,html,webmanifest,txt}"],
+//    swDest: "dist/sw.js",
+//    swSrc: "src-sw.js",
+// }
