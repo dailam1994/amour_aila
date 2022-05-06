30072e1cdc1af1484bca37af6ab9e27dcea29420
dailam1994 <dai_lam88@hotmail.com>
2022-05-06 16:56:57 +1000

    another commit

--- src/pages/admin/appointment/appointment.edit.jsx
+++ src/pages/admin/appointment/appointment.edit.jsx
@@ -143,6 +143,9 @@ const AppointmentEdit = () => {
       adminNewStartTime = new Date(adminFormatStartTime).toLocaleString().substring(12, 17)
    }
 
+   console.log(adminStartTime)
+   console.log(adminEndTime)
+
    // Submit Functionality
    const onSubmit = (formData) => {
       // Reset alert displays
